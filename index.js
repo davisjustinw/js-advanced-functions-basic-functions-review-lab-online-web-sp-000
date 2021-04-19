197e513bf984e1d7f39d965153d6582861aa5300
davisjustinw <davisjustinw@gmail.com>
2021-04-19 07:11:00 -0700

    commit tree 12b5a86ebb4f4681e2405019d80df7f177d20f1b

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 // Your code here
-let saturdayFun = ( activity = 'roller-skate' ) => {
+function saturdayFun(activity = 'roller-skate'){
   return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork = ( activity = 'go to the office' ) => {
-  return `This Monday, I will ${activity}.`
+const mondayWork = (activity = 'go to the office') => `This Monday, I will ${activity}.`
+
+function wrapAdjective( flair = '*' ) {
+  return ( adj = 'special' ) => `You are ${flair}${adj}${flair}!`
 }
 
-let wrapAdjective = ( flair = '*' ) => {
-  return ( adj = "special" ) => `You are ${flair}${adj}${flair}!`
-}
-
-let Calculator = {
+const Calculator = {
   add: () => 1 + 3,
   subtract: () => 1 - 3,
   multiply: () => 1 * 3,
   divide: () => 10 / 5
 }
 
-let actionApplyer = ( int, array ) => {
-  for ( let fun of array ) {
-    int = fun( int )
-  }
-  return int
+function actionApplyer( start, actions ) {
+  return actions.reduce( (result, action) => result = action( result ), start )
 }
